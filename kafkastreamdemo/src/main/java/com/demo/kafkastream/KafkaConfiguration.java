eec509a7ddd730f74051c130d3a11d641bbee3ee
root <root@gmail.com>
2019-04-25 22:36:46 +0530

    Code added.

--- kafkastreamdemo/src/main/java/com/demo/kafkastream/KafkaConfiguration.java
+++ kafkastreamdemo/src/main/java/com/demo/kafkastream/KafkaConfiguration.java
@@ -1,5 +1,7 @@
 package com.demo.kafkastream;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
@@ -28,26 +30,45 @@ public class KafkaConfiguration {
         return streamConfig;
     }
 
-    private  StreamsBuilder buildStream(String inTopic, String outTopic) {
+    private StreamsBuilder buildStream(final String bootstrapServers, final String inTopic, final String outTopic) {
+        final KafkaProducer<String, String> producer = createProducer(bootstrapServers);
         StreamsBuilder streamsBuilder = new StreamsBuilder();
         KStream<String, String> kStream = streamsBuilder.stream(inTopic);
 
-       kStream.filter(new Predicate<String, String>() {
-           public boolean test(String s, String s2) {
-               System.out.print("Event received");
-               return true;
-           }
-       }).to(outTopic);
-       return streamsBuilder;
+        kStream.filter(new Predicate<String, String>() {
+            public boolean test(String s, String s2) {
+                System.out.print("Event received. Sending using producer == ");
+                producer.send(new ProducerRecord<String, String>(outTopic, s, s2));
+                return true;
+            }
+        }).to(outTopic);
+        return streamsBuilder;
     }
 
     public void startStream(String inTopic, String outTopic, String bootstrapServers) {
-        final KafkaStreams kafkaStreams = new KafkaStreams(buildStream(inTopic, outTopic).build(), streamConfguration(bootstrapServers));
+        final KafkaStreams kafkaStreams = new KafkaStreams(buildStream(bootstrapServers, inTopic, outTopic).build(), streamConfguration(bootstrapServers));
         kafkaStreams.start();
         Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
             public void run() {
                 kafkaStreams.close();
             }
         }));
+    }
+
+    private KafkaProducer<String, String> createProducer(String bootstrapServers) {
+        return new KafkaProducer<String, String>(getProducerConfig(bootstrapServers));
+    }
+
+    private Properties getProducerConfig(String bootstrapServers) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", bootstrapServers);
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        return props;
     }
 }
